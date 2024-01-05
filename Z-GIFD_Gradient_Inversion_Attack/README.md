77a0de5d8b371591d0abf0eebc486224b5a9b85f
final-solution <2507586353@qq.com>
2024-01-05 14:59:45 +0800

    add: IPPA code

---
+++ Z-GIFD_Gradient_Inversion_Attack/README.md
@@ -0,0 +1,48 @@
+# GIFD: A Generative Gradient Inversion Method with Feature Domain Optimization
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+A PyTorch official implementation for [GIFD: A Generative Gradient Inversion Method with Feature Domain Optimization](), accepted to ICCV-2023.
+
+![pipeline](./figures/pipeline.png)
+
+## Results
+![results](./figures/results.jpg)
+
+## Setup
+We provide the environment configuration file exported by Anaconda, which can help you build up conveniently.
+```bash
+conda env create -f environment.yml
+conda activate GIFD 
+```  
+## Dataset and model file
+Download the [ImageNet](https://www.image-net.org/) and [FFHQ](https://github.com/NVlabs/ffhq-dataset) and provide their paths in the yml file.
+
+While the model weights of BigGAN are downloaded automatically, StyleGAN2 weights require downloaded manually as follows.
+
+`gdown --id 1c1qtz3MVTAvJpYvsMIR5MoSvdiwN2DGb` (shape predictor, placed in the root directory)
+
+`gdown --id 1JCBiKY_yUixTa6F1eflABL88T4cii2GR` (stylegan pre-trained checkpoint, placed in the inversefed\genmodels\stylegan2_io)
+
+## Quick start
+We prepare three configuration files for performing gradient inversion attacks, including the BigGAN-based, the StyleGAN2-based, and the GAN-free methods, where we give detailed descriptions of every parameter. Feel free to contact me at fang-h23@mails.tsinghua.edu.cn if you have any concerns.
+You can simply start by specifying the path of the config file.
+
+`python rec_mult.py --config $CONFIG_PATH`
+
+## Citation
+@inproceedings{fang2023gifd,
+  title={GIFD: A Generative Gradient Inversion Method with Feature Domain Optimization},
+  author={Fang, Hao and Chen, Bin and Wang, Xuan and Wang, Zhi and Xia, Shu-Tao},
+  booktitle={Proceedings of the IEEE/CVF International Conference on Computer Vision},
+  pages={4967--4976},
+  year={2023}
+}
+
+## Acknowledgement
+Our code is based on [invertingGradients](https://github.com/JonasGeiping/invertinggradients) and [ILO](https://github.com/giannisdaras/ilo) and we are grateful for their great devotion.
+
+For BigGAN, we use PyTorch official [implementation and weights](https://github.com/rosinality/stylegan2-pytorch).
+
+For StyleGAN2, we adapt this [Pytorch implementation](https://github.com/rosinality/stylegan2-pytorch), which is based on the [official Tensorflow code](https://github.com/NVlabs/stylegan2).
+
+We express great gratitude for their contribution to our community!
---
+++ attacks_intermediate/optimize.py
@@ -0,0 +1,168 @@
+from losses.poincare import poincare_loss
+from utils_intermediate.stylegan import project_onto_l1_ball
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class Optimization():
+    def __init__(self, target_model, synthesis, discriminator, transformations, num_ws, config):
+        self.synthesis = synthesis
+        self.target = target_model
+        self.discriminator = discriminator
+        self.config = config
+        self.transformations = transformations
+        self.discriminator_weight = self.config.attack['discriminator_loss_weight']
+        self.num_ws = num_ws
+        self.clip = config.attack['clip']
+        self.mid_vector = [None]      # 中间层的向量
+
+    def optimize(self, w_batch, targets_batch, num_epochs):
+        print("-------------start intermidiate space search---------------")
+
+        # 每一轮就是搜一层
+        for i, steps in enumerate(self.config.intermediate['steps']):
+            torch.cuda.empty_cache()
+            start_layer = i + self.config.intermediate['start']
+            if i > self.config.intermediate['end']:
+                raise Exception('Attemping to go after end layer')
+            imgs, w_batch = self.intermediate(
+                w_batch, start_layer, targets_batch, steps, i)
+
+        return imgs, w_batch.detach()
+
+    # 定义中间层搜索一层的函数
+    def intermediate(self, w, start_layer, targets_batch, steps, index):
+        print(
+            f'---------search Layer {start_layer} in {steps} iterations---------')
+
+        # 中间层搜索前的准备工作
+        with torch.no_grad():
+            if start_layer == 0:
+                var_list = [w.requires_grad_()]
+            else:
+                self.mid_vector[-1].requires_grad = True
+                var_list = [w.requires_grad_()] + [self.mid_vector[-1]]
+                prev_mid_vector = torch.ones(
+                    self.mid_vector[-1].shape, device=self.mid_vector[-1].device) * self.mid_vector[-1]
+            prev_w = torch.ones(w.shape, device=w.device) * w
+            self.synthesis.module.set_layer(start_layer, self.config.intermediate['end'])
+
+        # 设置优化器
+        optimizer = self.config.create_optimizer(params=var_list)
+        scheduler = self.config.create_lr_scheduler(optimizer)
+
+        # 开始中间层搜索
+        for i in range(steps):
+            imgs = self.synthesize(
+                w, layer_in=self.mid_vector[-1], num_ws=self.num_ws)
+
+            # compute discriminator loss
+            if self.discriminator_weight > 0:
+                discriminator_loss = self.compute_discriminator_loss(
+                    imgs)
+            else:
+                discriminator_loss = torch.tensor(0.0)
+
+            # perform image transformations
+            if self.clip:
+                imgs = self.clip_images(imgs)
+            if self.transformations:
+                imgs = self.transformations(imgs)
+
+            # Compute target loss
+            outputs = self.target(imgs)
+            target_loss = poincare_loss(
+                outputs, targets_batch).mean()
+
+            # combine losses and compute gradients
+            optimizer.zero_grad()
+            loss = target_loss + discriminator_loss * self.discriminator_weight
+            loss.backward()
+            optimizer.step()
+
+            if scheduler:
+                scheduler.step()
+
+            # 限制在L1球内，包括中间层和w-space隐向量
+            if start_layer != 0 and self.config.intermediate['max_radius_mid_vecor'][index] > 0:
+                deviation = project_onto_l1_ball(self.mid_vector[-1] - prev_mid_vector,
+                                                 self.config.intermediate['max_radius_mid_vecor'][index])
+                var_list[-1].data = (prev_mid_vector + deviation).data
+            if self.config.intermediate['max_radius_w'][index] > 0:
+                deviation = project_onto_l1_ball(w - prev_w,
+                                                 self.config.intermediate['max_radius_w'][index])
+                var_list[0].data = (prev_w + deviation).data
+
+            # Log results
+            if self.config.log_progress:
+                with torch.no_grad():
+                    confidence_vector = outputs.softmax(dim=1)
+                    confidences = torch.gather(
+                        confidence_vector, 1, targets_batch.unsqueeze(1))
+                    mean_conf = confidences.mean().detach().cpu()
+
+                if torch.cuda.current_device() == 0 and (i+1) % 10 == 0:
+                    print(
+                        f'iteration {i}: \t total_loss={loss:.4f} \t target_loss={target_loss:.4f} \t',
+                        f'discriminator_loss={discriminator_loss:.4f} \t mean_conf={mean_conf:.4f}'
+                    )
+
+            torch.cuda.empty_cache()
+
+        # 搜索完成，为下一层的搜索做准备
+        with torch.no_grad():
+            self.synthesis.module.set_layer(start_layer, start_layer)
+            w_expanded = torch.repeat_interleave(w,
+                                                 repeats=self.num_ws,
+                                                 dim=1)
+            mid_vector, _ = self.synthesis(
+                w_expanded, layer_in=self.mid_vector[-1], noise_mode='const', force_fp32=True)
+            self.mid_vector.append(mid_vector)
+            self.synthesis.module.set_layer(start_layer, self.config.intermediate['end'])
+
+        return imgs, w.detach()
+
+    # 中间层搜索用的图片合成函数
+    def synthesize(self, w, layer_in, num_ws):
+        if w.shape[1] == 1:
+            # 先扩展w
+            w_expanded = torch.repeat_interleave(w,
+                                                 repeats=num_ws,
+                                                 dim=1)
+            imgs = self.synthesis(w_expanded,
+                                  layer_in=layer_in,
+                                  noise_mode='const',
+                                  force_fp32=True)
+        else:
+            imgs = self.synthesis(w, layer_in=layer_in,
+                                  noise_mode='const', force_fp32=True)
+        return imgs
+
+    # def synthesize(self, w, num_ws):
+    #     if w.shape[1] == 1:
+    #         # 先扩展w
+    #         w_expanded = torch.repeat_interleave(w,
+    #                                              repeats=num_ws,
+    #                                              dim=1)
+    #         imgs = self.synthesis(w_expanded,
+    #                               noise_mode='const',
+    #                               force_fp32=True)
+    #     else:
+    #         imgs = self.synthesis(w, noise_mode='const', force_fp32=True)
+    #     return imgs
+
+    def clip_images(self, imgs):
+        lower_limit = torch.tensor(-1.0).float().to(imgs.device)
+        upper_limit = torch.tensor(1.0).float().to(imgs.device)
+        imgs = torch.where(imgs > upper_limit, upper_limit, imgs)
+        imgs = torch.where(imgs < lower_limit, lower_limit, imgs)
+        return imgs
+
+    def compute_discriminator_loss(self, imgs):
+        discriminator_logits = self.discriminator(imgs, None)
+        discriminator_loss = nn.functional.softplus(
+            -discriminator_logits).mean()
+        return discriminator_loss

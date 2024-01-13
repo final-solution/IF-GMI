fix: bugs of prcd calculation

intermediate_attack.py
@@ -67,7 +67,7 @@ def main():
 
     # Set devices: 设备驱动
     torch.set_num_threads(24)
-    os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     gpu_devices = [i for i in range(torch.cuda.device_count())]
 
@@ -382,7 +382,7 @@ def main():
                 # compute precision, recall, density, coverage: 计算指标
                 prcd.set(training_dataset, attack_dataset)
                 prcd.compute_metric(
-                    layer, final_targets[0], k=3, rtpt=rtpt)
+                    layer, int(final_targets[0]), k=3, rtpt=rtpt)
 
         except Exception:
             print(traceback.format_exc())

metrics_intermediate/prcd.py
@@ -112,7 +112,7 @@ class PRCD:
 
     def compute_embedding(self, dataset, cls=None, fake=False):
         self.inception_model.eval()
-        if cls:
+        if cls is not None:
             dataset = SingleClassSubset(dataset, cls)
         dataloader = torch.utils.data.DataLoader(dataset,
                                                  batch_size=self.batch_size,
@@ -120,6 +120,7 @@ class PRCD:
                                                  drop_last=False,
                                                  pin_memory=True,
                                                  num_workers=self.num_workers)
+        print('dataset size:',len(dataset))
         pred_arr = np.empty((len(dataset), self.dims))
         start_idx = 0
         max_iter = int(len(dataset) / self.batch_size)
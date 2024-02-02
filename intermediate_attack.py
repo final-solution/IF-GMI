3847fabe200c01cba823e9385fc58e44f21c2804
final-solution <2507586353@qq.com>
2024-02-02 13:54:49 +0800

    fix: less memory

--- intermediate_attack.py
+++ intermediate_attack.py
@@ -49,7 +49,7 @@ def main():
 
     # Set devices: 设备驱动
     torch.set_num_threads(24)
-    os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '2,3'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     gpu_devices = [i for i in range(torch.cuda.device_count())]
 
